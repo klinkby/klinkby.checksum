4b0d87e77999f5044e3f2b56267e461321b4edaf
klinkby <m@kli.dk>
2014-11-15 20:21:20 +0100

    Added implementation, nuspec.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Klinkby.Checkum")]
+[assembly: AssemblyDescription("Fast CRC32 implementation for .NET Framework 4.5.1. ** NOT FOR SECURITY USE **")]
